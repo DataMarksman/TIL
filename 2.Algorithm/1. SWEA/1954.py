86a374e74dcf51da93f2aa6b3b4e04aa26ba9cfb
DataMarksman <zkstizn@gmail.com>
2022-07-23 23:17:43 +0900

    0723_commit

---
+++ 2.Algorithm/1. SWEA/1954.py
@@ -0,0 +1,25 @@
+case_count = int(1)
+
+num = int(input())
+
+snail_list = [[0]*num]*num    # 0으로 이루어진 num x num의 2차원 배열 생성
+digit = int(1)
+print('#',str(case_count))
+t = int(0)
+r_num = int(num/2 if num % 2 == 0 else (num+1)/2) +1
+for t in range(r_num):
+    for i in range(num-2*t):
+        snail_list[i+t][t] = digit
+        digit += 1
+    for j in range(num-2*t):
+        snail_list[(num-2*t)-1][j+t] = digit
+        digit += 1
+    for p in range(num-2*t):
+        snail_list[(num-2*t)-1-p][num-1-t] = digit
+        digit += 1
+    for q in range(num-2*t):
+        snail_list[t][(num-t)-1-q] = digit
+        digit += 1
+
+case_count += 1
+print(snail_list)
