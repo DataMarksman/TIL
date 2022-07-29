64840e57cc35c943a82ece965c88ce1629e65f3d
DataMarksman <zkstizn@gmail.com>
2022-07-29 23:29:31 +0900

    1859_added

---
+++ 2.Algorithm/1. SWEA/1859.py
@@ -0,0 +1,13 @@
+case = int(input()) # 전체 케이스 선언 
+for i in range(case): # 전체 케이스 만큼 돌리기
+    count = int(input()) # 케이스 별 요소 수 선언
+    for j in range(count):   # 케이스 별 요소 수만큼 반복
+           future_list = map(int,input().split())
+           for i in future_list
+
+
+
+
+    print(f'#{i+1}')
+
+
