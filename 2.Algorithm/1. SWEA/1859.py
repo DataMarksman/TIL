5344fa83aca8af5f0d9aa06c1a948269ca9d31ef
DataMarksman <zkstizn@gmail.com>
2022-07-31 00:52:08 +0900

    0731_added

--- 2.Algorithm/1. SWEA/1859.py
+++ 2.Algorithm/1. SWEA/1859.py
@@ -1,13 +1,25 @@
 case = int(input()) # 전체 케이스 선언 
 for i in range(case): # 전체 케이스 만큼 돌리기
     count = int(input()) # 케이스 별 요소 수 선언
-    for j in range(count):   # 케이스 별 요소 수만큼 반복
-           future_list = map(int,input().split())
-           for i in future_list
+    for j in range(count):
+       future_list = list(map(int,input().split()))
+       answer_list = []
+       while_con = True
+       while while_con == True:
+              answer_stack = int(0)
+              max_num = max(future_list)
+              max_po = future_list.index(max_num)
+              answer_list = future_list[:int(max_po)]
+              if answer_list == future_list[0]:
+                     while_con = False
+              else:
+                     del future_list[:int(max_po)]
+                     answer_list
 
 
 
 
-    print(f'#{i+1}')
+
+       print(f'#{i+1}')
 
 
--- 2.Algorithm/1. SWEA/1946.py
+++ 2.Algorithm/1. SWEA/1946.py
@@ -1,12 +1,23 @@
 # 이 문제의 요점은 즉 특정한 규칙으로 정리되어있는 자료들을 원래의 상태로 나열하는 것.
-case_count = int(input()) # 처음에 주어지는 케이스의 수는, 압축된 자료의 개수
 
-for i in range(1,case_count + 1): # 이하의 루틴을 주어진 케이스의 수만큼 반복 출력
-    case_num = int(input()) # 그 다음에 각 자료별로 주어지는 케이스의 수를 제시함
+# 처음에 주어지는 input 값은, 압축된 자료의 개수
+case_count = int(input()) 
+
+# 이하의 루틴을 자료의 수만큼 반복 출력
+for i in range(1,case_count + 1): 
+
+    # 그 다음에 각 자료별로 주어지는 케이스의 수를 제시함 + 스톡 저장을 위한 빈공간 설계
+    case_num = int(input()) 
     stocks = ''
-    for j in range(case_num):  # 그 내용을 출력 규격에 맞게 출력하기.
+
+    # 그 내용을 출력 규격에 맞게 스톡에 저장
+    for j in range(case_num):
         text, num = input().split()
         stocks += text*int(num)
+    
+    # 현재 자료의 순번을 출력하고
     print('#'+str(i))
+
+    # 10줄씩 잘라서 스톡에 저장된 내용을 출력
     for k in range(0,len(stocks),10): 
         print(stocks[k:k+10])
