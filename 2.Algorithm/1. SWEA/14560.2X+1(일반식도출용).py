a1dff5d053fe4249deda3c3d91947cd6c46e7696
DataMarksman <zkstizn@gmail.com>
2022-08-25 01:29:17 +0900

    '2022-08-25_commit'

--- 2.Algorithm/1. SWEA/14560.2X+1(일반식도출용).py
+++ 2.Algorithm/1. SWEA/14560.2X+1(일반식도출용).py
@@ -10,10 +10,28 @@ B_N = num_list[3]
 B_C = num_list[4]
 """
 
+# 제조기 만들기:
+# x, y 두가지로 구성된 리스트.
+# 이것을 0,1로 바꿔서 순열로 구성한다. 단, 0의 개수와 1의 개수는 동일하다.
+# 즉, 2*n개의 요소를 가진다. depth 가 1 커질 수록 길이는 2씩 증가.
+
+def finding(n):
+    global ans_list
+    flag = True
+    while flag:
+        count_xy = [n, n]
+        for pick in range(2):
+            count_xy[pick] -= 1
 
 
 
 
+
+
+    if (B_C-A_C)/A_P == 7:
+        return ans_list
+    else:
+        finding(n+1)
 
 A_N = 1
 A_P = 1
@@ -23,6 +41,7 @@ B_N = 1
 B_C = 0
 print(f'A: {A_N}N + {A_P}P + {A_C}, B: {B_N}N + {B_C}')
 
+ans_list = []
 
 while True:
     key = input()
