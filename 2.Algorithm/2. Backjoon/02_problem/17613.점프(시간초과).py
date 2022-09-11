b39765de6b5e464e7833f39df7efea3c60a9ab25
DataMarksman <zkstizn@gmail.com>
2022-09-11 23:09:47 +0900

    '2022-09-11_commit'

--- 2.Algorithm/2. Backjoon/02_problem/17613.점프(시간초과).py
+++ 2.Algorithm/2. Backjoon/02_problem/17613.점프(시간초과).py
@@ -4,32 +4,41 @@
 # import sys
 # sys.setrecursionlimit(10**6)
 
+# memo = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024,
+#         2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576,
+#         2097152, 4194304, 8388608, 16777216, 33554432, 67108864, 134217728, 268435456, 536870912, 1073741824,
+#         2147483648, 4294967296, 8589934592, 17179869184, 34359738368, 68719476736, 137438953472]
+
+#
+# def jumping(t):
+#     global max_ans
+#     count = 0
+#     while True:
+#         for jump in range(37):
+#             if memo[jump]-1 > t:
+#                 count += jump-1
+#                 t -= (memo[jump-1]-1)
+#                 break
+#         if t <= 0:
+#             break
+#     if count > max_ans:
+#         max_ans = count
+#
+#
+# T = int(input())
+# for case_num in range(1, T+1):
+#     N, M = tuple(map(int, input().split()))
+#     size = M-N+1
+#     max_ans = 0
+#     for x in range(size):
+#         jumping(int(x+N))
+#     print(max_ans)
+
 memo = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024,
         2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576,
         2097152, 4194304, 8388608, 16777216, 33554432, 67108864, 134217728, 268435456, 536870912, 1073741824,
         2147483648, 4294967296, 8589934592, 17179869184, 34359738368, 68719476736, 137438953472]
 
 
-def jumping(t):
-    global max_ans
-    count = 0
-    while True:
-        for jump in range(37):
-            if memo[jump]-1 > t:
-                count += jump-1
-                t -= (memo[jump-1]-1)
-                break
-        if t <= 0:
-            break
-    if count > max_ans:
-        max_ans = count
-
-
-T = int(input())
-for case_num in range(1, T+1):
-    N, M = tuple(map(int, input().split()))
-    size = M-N+1
-    max_ans = 0
-    for x in range(size):
-        jumping(int(x+N))
-    print(max_ans)
+# k 는 비교할 상수, count_J는 이미 점프한 횟수, start는 이전 점프대 위치
+def jumping(k)
