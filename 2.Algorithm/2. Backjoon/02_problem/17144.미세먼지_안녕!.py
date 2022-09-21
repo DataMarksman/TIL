92037af3bb660450616df39e523451aa0c951503
DataMarksman <zkstizn@gmail.com>
2022-09-22 01:23:32 +0900

    '2022-09-22_commit'

--- 2.Algorithm/2. Backjoon/02_problem/17144.미세먼지_안녕!.py
+++ 2.Algorithm/2. Backjoon/02_problem/17144.미세먼지_안녕!.py
@@ -1,8 +1,7 @@
 # BOJ.
 # 설계 의도: 조건에 맞는 실행
 # 개선점:
-# import sys
-# sys.setrecursionlimit(10**6)
+import copy
 dx = [0, 0, -1, 1]
 dy = [1, -1, 0, 0]
 
@@ -11,16 +10,12 @@ height = int(R)
 wide = int(C)
 board = []
 start = []
-ans = 0
 for put_in in range(R):
     lines = list(map(int, input().split()))
     if lines[0] == -1:
         start += [put_in]
-    else:
-        ans += sum(lines)
     board += [lines]
 turn_count = 0
-print(board)
 while turn_count < T:
     re_board = [[0]*wide for _ in range(height)]
     for x in range(height):
@@ -34,30 +29,35 @@ while turn_count < T:
                     if 0 <= px < height and 0 <= py < wide and board[px][py] != -1:
                         count_x += 1
                         re_board[px][py] += dust//5
-            re_board[x][y] += dust - (dust//5 * count_x)
-    ans -= re_board[start[0]-1][0]
-    ans -= re_board[start[1]+1][0]
+                re_board[x][y] += (dust - (dust//5 * count_x))
+    re_board[start[0]-1][0] = 0
+    re_board[start[1]+1][0] = 0
     for rearrange_A in range(start[0]-1, 0, -1):
         re_board[rearrange_A][0] = int(re_board[rearrange_A - 1][0])
-    for rearrange_B in range(start[1]+1, height-2):
+    for rearrange_B in range(start[1]+1, height-1):
         re_board[rearrange_B][0] = int(re_board[rearrange_B + 1][0])
 
-    for rearrange_C in range(wide - 2):
+    for rearrange_C in range(wide - 1):
         re_board[0][rearrange_C] = int(re_board[0][rearrange_C+1])
         re_board[height - 1][rearrange_C] = int(re_board[height - 1][rearrange_C+1])
 
-    for rearrange_D in range(0, start[0]-1):
+    for rearrange_D in range(0, start[0]):
         re_board[rearrange_D][wide - 1] = int(re_board[rearrange_D + 1][wide - 1])
-    for rearrange_E in range(height-1, start[1]+1, -1):
+    for rearrange_E in range(height-1, start[1], -1):
         re_board[rearrange_E][wide - 1] = int(re_board[rearrange_E - 1][wide - 1])
 
     for rearrange_F in range(wide - 1, 1, -1):
         re_board[start[0]][rearrange_F] = int(re_board[start[0]][rearrange_F - 1])
         re_board[start[1]][rearrange_F] = int(re_board[start[1]][rearrange_F - 1])
-
-    board = re_board
-    print(board)
+    re_board[start[0]][1] = 0
+    re_board[start[1]][1] = 0
+    re_board[start[0]][0] = -1
+    re_board[start[1]][0] = -1
+    board = copy.deepcopy(re_board)
     turn_count += 1
-print(board)
+ans = 2
+for rx in range(height):
+    for ry in range(wide):
+        ans += board[rx][ry]
 print(ans)
 
