12613374ab21f968cb4c972d3104c7729817bfe5
DataMarksman <zkstizn@gmail.com>
2022-10-06 22:54:44 +0900

    '2022-10-06_commit'

--- 2.Algorithm/2. Backjoon/02_problem/1089.스타트링크 타워.py
+++ 2.Algorithm/2. Backjoon/02_problem/1089.스타트링크 타워.py
@@ -1,16 +1,53 @@
 # BOJ.
 # 설계 의도: 조건에 맞는 실행
 # 개선점:
-# import sys
-# sys.setrecursionlimit(10**6)
 
-def searching(idx):
-    num_set = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, }
-    if board[0][idx+1] == '#':
-        num_set.remove()
+"""
+
+###...#.###.###.#.#.###.###.###.###.###
+#.#...#...#...#.#.#.#...#.....#.#.#.#.#
+#.#...#.###.###.###.###.###...#.###.###
+#.#...#.#.....#...#...#.#.#...#.#.#...#
+###...#.###.###...#.###.###...#.###.###
+
+"""
+
+import sys
+input = sys.stdin.readline
+check_set = [[{0,2,3,4,5,6,7,8,9,}, {0,2,3,5,6,7,8,9,}, {0,1,2,3,4,5,6,7,8,9,}],
+             [{0,4,5,6,8,9,}, set(), {0,1,2,3,4,7,8,9,}],
+             [{0,2,3,4,5,6,8,9,}, {2,3,4,5,6,8,9,}, {0,1,2,3,4,5,6,7,8,9,}],
+             [{0,2,6,8,}, set(), {0,1,3,4,5,6,7,8,9,}],
+             [{0,2,3,5,6,8,9,}, {0,2,3,5,6,8,9,}, {0,1,2,3,4,5,6,7,8,9,}]]
 
 
-
+num_list = [1,2,3,4,5,6,7,8,9,0]
 N = int(input())
-board = [list(input()) for _ in range(N)]
-numbers = [[] for _ in range(N)]
+my_number = [set(num_list) for j in range(N)]
+board = [[] for _ in range(N)]
+for put_in in range(5):
+    line = list(input())
+    for checking in range(N):
+        for window in range(3):
+            if my_number[checking]:
+                if line[(checking*4) + window] == '#':
+                    my_number[checking] &= check_set[put_in][window]
+            else:
+                break
+count = 1
+for first_check in range(N):
+    if not my_number[first_check]:
+        ans = -1
+        break
+    else:
+        count *= len(my_number[first_check])
+else:
+    ans = 0
+    for summing in range(N):
+        multiple = count / len(my_number[summing])
+        while my_number[summing]:
+            pick = my_number[summing].pop()
+            ans += pick*(10**((N-1)-summing))*multiple
+    ans /= count
+print(ans)
+
--- 2.Algorithm/2. Backjoon/02_problem/1516.게임 개발.py
+++ 2.Algorithm/2. Backjoon/02_problem/1516.게임 개발.py
@@ -1,29 +1,13 @@
 # BOJ.
-# 설계 의도: 조건에 맞는 실행
+# 설계 의도: DFS로 구현하는 위상정렬
 # 개선점:
 import sys
 import heapq
 # sys.setrecursionlimit(10**6)
-# input = sys.stdin.readline
-# import sys
-# input = sys.stdin.readline
-# N = int(input())
-# num_list = [0]*10001
-# for put_in in range(N):
-#     num_list[int(input())] += 1
-# for printing in range(1, 10001):
-#     if num_list[printing]:
-#         for p in range(num_list[printing]):
-#             print(printing)
+input = sys.stdin.readline
 
-Money = int(input())
+
 N = int(input())
-ans = 0
-for put_in in range(N):
-    A, B = map(int, input().split())
-    ans += A * B
-if Money == ans:
-    print('Yes')
-else:
-    print('No')
+
+
 
