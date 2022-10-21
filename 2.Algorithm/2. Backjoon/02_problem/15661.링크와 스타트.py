a0c2c922d784e3e32f33459b01ac6c30eccf3ce7
DataMarksman <zkstizn@gmail.com>
2022-10-21 17:58:58 +0900

    '2022-10-21_commit'

--- 2.Algorithm/2. Backjoon/02_problem/15661.링크와 스타트.py
+++ 2.Algorithm/2. Backjoon/02_problem/15661.링크와 스타트.py
@@ -1,35 +1,34 @@
-# BOJ.
+# BOJ.15661. 링크와 스타트
 # 설계 의도: 조건에 맞는 실행
 # 개선점:
 import sys
+from itertools import combinations
+import heapq
 sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
-def case_sort(K, pick_set):
-    global ans
-    pick_set = set(pick_set)
-    if len(pick_set) == N // 2:
-        A = list(pick_set)
-        B = list(num_set-pick_set)
-        sum_A = 0
-        sum_B = 0
-        for i in range((N//2)-1):
-            for j in range(i+1, N//2):
-                sum_A += board[A[i]][A[j]] + board[A[j]][A[i]]
-                sum_B += board[B[i]][B[j]] + board[B[j]][B[i]]
-        if abs(sum_A - sum_B) < ans:
-            ans = abs(sum_A - sum_B)
-    else:
-        for pick in range(K+1, N):
-            if pick not in pick_set:
-                pick_set.add(pick)
-                case_sort(pick, pick_set)
-                pick_set.remove(pick)
-
 
 N = int(input())
-num_set = set(list(i for i in range(N)))
 board = [list(map(int, input().split())) for _ in range(N)]
+num_dict = {}
+for x in range(N):
+    for y in range(N):
+        if x < y:
+            num_dict[(x, y)] = int(board[x][y] + board[y][x])
+iter_set = set(combinations((i for i in range(1, N+1)), j) for j in range(1, N+1))
+visited = set()
+while iter_set:
+    temp_ans = 0
+    temp_list = list(iter_set.pop())
+    for i in range(len(temp_list)-1):
+        for j in range(i, len(temp_list)):
+            temp_ans += num_dict[(i, j)]
+            if temp_ans >
+
+
+
+
+
 ans = 99999999999999
 case_sort(0, set())
 print(ans)
