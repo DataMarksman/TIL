dbb803f3abe380afb9ca717918d60ba66c4540b9
DataMarksman <zkstizn@gmail.com>
2022-10-24 17:58:43 +0900

    '2022-10-24_commit'

---
+++ JS/01.types.js
@@ -0,0 +1,29 @@
+// 데이터 종류 (자료형) -> JS는 데이터 기준으로 사고 함
+
+/*
+    Primitive Types
+    1. Number
+    2. String
+    3. Empty
+    4. Boolean
+
+*/
+
+let myName = 'kyou'
+let greeting = `Hello, my name is ${myName}`
+console.log(greeting)
+console.log(안녕 +하세요)
+
+// number
+console.log(
+    'Number Types: ',
+    Infinity, -Infinity, NaN
+)
+
+// Empty Values
+console.log(undefined, null)
+
+// Boolean Types
+console.log(true, false)
+
+
---
+++ JS/05.functions.js
@@ -0,0 +1,12 @@
+
+
+
+// 1. 선언 식
+// -> 선언식 function 함수 이름 () {} === 기명 함수
+
+function add(x, y) {
+    return x + y
+}
+
+let a = add(3, 4)
+console.log(a)
