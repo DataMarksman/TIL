b232bac167272cc99bfc8292718f1ebb799ce6e3
DataMarksman <zkstizn@gmail.com>
2022-10-26 22:15:43 +0900

    '2022-10-26_commit'

---
+++ 2.Algorithm/2. Backjoon/02_problem/2098.외판원 순회 1.py
@@ -0,0 +1,35 @@
+# BOJ.
+# 설계 의도: 조건에 맞는 실행
+# 개선점:
+import sys
+# sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+N = int(input())
+num_list = list(map(int, input().split()))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
