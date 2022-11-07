5ca3bcc56f1fb42a6c319364e7b87c0bc16ec21a
DataMarksman <zkstizn@gmail.com>
2022-11-07 17:27:08 +0900

    '2022-11-07_commit'

---
+++ 4. Language/Java Script/02. vue_cli/lifecyclehook/src/components/DogComponent.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <button @click="getDogImage">멍멍아 이리온</button><br><br>
+    <img v-if="imgSrc" :src="imgSrc" alt="#"><br>
+  </div>
+</template>
+
+
+<script>
+import axios from 'axios'
+
+export default {
+  name:'DogComponent',
+  data() {
+    return {
+      imgSrc: null,
+    }
+  },
+  methods:{
+    getDogImage() {
+      const dogImageSearchURL = 'https://dog.ceo/api/breeds/image/random'
+      
+      axios({
+        method: 'get',
+        url: dogImageSearchURL
+      })
+        .then((response) => {
+          const imgSrc = response.data.message
+          this.imgSrc = imgSrc
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
+  },
+  created() {
+    this.getDogImage()
+    // const button = document.querySelctor('button')
+    // button.innerText = 'ㅁㅇㅁㅇ'
+  },
+  mounted() {
+    const button = document.querySelctor('button')
+    button.innerText = '멍멍!'
+
+  }
+}
+</script>
+
+<style>
+
+</style>
