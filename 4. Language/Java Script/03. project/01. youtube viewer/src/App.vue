a792a39505614d1e782a77af6984a5b0a7ca3f71
DataMarksman <zkstizn@gmail.com>
2022-11-08 17:58:54 +0900

    '2022-11-08_commit'

---
+++ 4. Language/Java Script/03. project/01. youtube viewer/src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="app" class="container vuetube-wrap ">
+    <!-- 제목 영역 -->
+    <header class="d-felx justify-content-center">
+      <h1 class="'mt-4 text-primary">SSAFY TUBE</h1>
+    </header>
+
+    <!-- 검색바 -->
+    <SearchBar @fetch-videos='onFetchVideos'/>
+
+    <!-- 본문 -->
+    <section class="row mt-4">
+      <!-- 비디오 플레이어 영역 -->
+      <div class="mt-4">
+        <VideoDetail :selectedVideo="selectedVideo" />
+      </div>
+      <!-- 비디오 리스트 영역 (5개) -->
+      <div>
+        <VideoList :videos="videos" @select="onSelect" />
+      </div>
+
+    </section>
+  </div>
+</template>
+
+
+<!-- APP.vue: 특정 하나의 비디오 정보, 비디오 리스트 가짐  -->
+<!-- VideoDetail 에서 비디오 하나, 다 -->
+ 
+<script>
+// 스크립트 작성 순서: import -> 명시 -> 사용
+import SearchBar from '@/components/SearchBar.vue'
+import VideoDetail from '@/components/VideoDetail.vue'
+import VideoList from '@/components/VideoList.vue'
+// import axios from 'axios'
+
+export default {
+  name: 'App',
+  
+  components: {
+    SearchBar, VideoDetail, VideoList
+  },
+  data() {
+    return {
+      videos: [],
+      selectedVideo: {},
+    }
+  },
+  methods: {
+    onFetchVideos(videos) {
+      this.videos = videos
+    },
+    onSelect(selectedVideo) {
+      this.selectedVideo = selectedVideo
+    }
+  },
+  computed: {
+    isSelectedVideo() {
+      return !!Object.keys(this.selectedVideo).length
+    }
+  }
+}
+</script>
+
+<style>
+  .vuetube-wrap {
+    min-height: 100vh;
+  }
+</style>
