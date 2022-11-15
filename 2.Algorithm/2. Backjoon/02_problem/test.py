d8c5c8df8d602f4d5ea156bdc4fced41fa3c2573
DataMarksman <zkstizn@gmail.com>
2022-11-15 23:58:47 +0900

    '2022-11-15_commit'

--- 2.Algorithm/2. Backjoon/02_problem/test.py
+++ 2.Algorithm/2. Backjoon/02_problem/test.py
@@ -569,10 +569,43 @@
 # print(sorted(original))
 
 
+#
+# import sys
+# input = sys.stdin.readline
+# A, B = map(int, input().split())
+# ans = A // B
+# print(ans)
+# print(A - (ans*B))
+
 
 import sys
 input = sys.stdin.readline
-A, B = map(int, input().split())
-ans = A // B
-print(ans)
-print(A - (ans*B))
+from collections import deque
+n = int(input())
+q = deque([])
+for i in range(n):
+    s = input().split()
+    if s[0] == 'push':
+        q.append(s[1])
+    elif s[0] == 'pop':
+        if not q:
+            print(-1)
+        else:
+            print(q.popleft())
+    elif s[0] == 'size':
+        print(len(q))
+    elif s[0] == 'empty':
+        if not q:
+            print(1)
+        else:
+            print(0)
+    elif s[0] == 'front':
+        if not q:
+            print(-1)
+        else:
+            print(q[0])
+    elif s[0] == 'back':
+        if not q:
+            print(-1)
+        else:
+            print(q[-1])
