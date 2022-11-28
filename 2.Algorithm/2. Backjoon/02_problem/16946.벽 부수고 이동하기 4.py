2fe5ff09bdf0787a4b3f366795e1e49da68121b8
DataMarksman <zkstizn@gmail.com>
2022-11-29 01:47:04 +0900

    '2022-11-29_commit'

--- 2.Algorithm/2. Backjoon/02_problem/16946.벽 부수고 이동하기 4.py
+++ 2.Algorithm/2. Backjoon/02_problem/16946.벽 부수고 이동하기 4.py
@@ -2,7 +2,6 @@
 # 설계 의도: 조건에 맞는 실행
 # 개선점:
 import sys
-# sys.setrecursionlimit(10**6)
 
 dx = [0, 0, 1, -1]
 dy = [1, -1, 0, 0]
@@ -12,14 +11,14 @@ dy = [1, -1, 0, 0]
 N, M = map(int, sys.stdin.readline().strip().split())
 
 board = [list(sys.stdin.readline().strip()) for _ in range(N)]
-value_board = [[0]*M for _ in range(N)]
 visited = set()
-
-
+visited_list = []
+value_list = []
 for i in range(N):
     for j in range(M):
-        if board[i][j] == '0':
+        if board[i][j] == '0' and (i, j) not in visited:
             temp_visited = {(i, j), }
+            visited.add((i, j))
             stack = {(i, j), }
             while stack:
                 pick = stack.pop()
@@ -29,30 +28,31 @@ for i in range(N):
                     PX = X + dx[direction]
                     PY = Y + dy[direction]
                     if 0 <= PX < N and 0 <= PY < M and (PX, PY) not in visited:
-                        if board[PX][PY] == 0:
+                        if board[PX][PY] == '0':
                             temp_visited.add((PX, PY))
-                            visited.add((PX, PY))
                             stack.add((PX, PY))
-            Step_count = int(len(temp_visited))
-            while temp_visited:
-                pick_step = temp_visited.pop()
-                R = pick_step[0]
-                C = pick_step[1]
-                value_board[R][C] = Step_count
+            visited_list.append(set(temp_visited))
+            value_list.append(int(len(temp_visited)))
 
 for x in range(N):
     for y in range(M):
-        if int(board[x][y]) > 0:
-            temp_value = 1
+        if board[x][y] == '1':
+            value_set = set()
             for direction in range(4):
                 px = x + dx[direction]
                 py = y + dy[direction]
                 if (px, py) in visited:
-                    temp_value += value_board[px][py]
+                    for checking in range(len(value_list)):
+                        if (px, py) in visited_list[checking]:
+                            value_set.add(checking)
             else:
-                board[x][y] = str(int(temp_value//10))
-print(value_board)
-print(board)
+                board_value = 1
+                while value_set:
+                    pick = value_set.pop()
+                    board_value += value_list[pick]
+                board[x][y] = str(board_value%10)
+for printing in range(N):
+    print("".join(board[printing]))
 
 
 
