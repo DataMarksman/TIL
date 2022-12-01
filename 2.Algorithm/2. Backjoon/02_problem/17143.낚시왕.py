38020f4b32394bbeac5b24bb1e08a2ecdfa08f1c
DataMarksman <zkstizn@gmail.com>
2022-12-01 23:51:19 +0900

    '2022-12-01_commit'

--- 2.Algorithm/2. Backjoon/02_problem/17143.낚시왕.py
+++ 2.Algorithm/2. Backjoon/02_problem/17143.낚시왕.py
@@ -14,8 +14,4 @@ for sharks in range(M):
 
 num_list = list(map(int, input().split()))
 
-N = int(input())
-for case_num in range(1,N+1):
 
-    T = int(input())
-    for case_num in range(1, T + 1):
