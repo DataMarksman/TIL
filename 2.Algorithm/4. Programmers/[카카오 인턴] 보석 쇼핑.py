8e90344e9b9e32d04df0c15e62b51063f1822d8f
DataMarksman <zkstizn@gmail.com>
2023-01-09 01:47:07 +0900

    '2023-01-09_commit'

--- 2.Algorithm/4. Programmers/[카카오 인턴] 보석 쇼핑.py
+++ 2.Algorithm/4. Programmers/[카카오 인턴] 보석 쇼핑.py
@@ -1,5 +1,5 @@
 # PRG.
-
+# https://school.programmers.co.kr/learn/courses/30/lessons/67258
 # 설계 의도: 앞에서부터 딱 한 번씩만 체크해서 최적의 답을 보자구요!
 
 # 로직의 Main 개념: semi DP
@@ -17,103 +17,54 @@
 
 
 def solution(gems):
-    # 보석의 총 길이를 변수에 넣어줍니다.
-    length = len(gems)
-
-    # 계속 경신 시켜줄 answer 판단용 최소 길이 값 입니다.
-    min_length = int(length)
-
-    # answer로 반환 시킬, [시작 idx, 끝 idx] 의 초기값 입니다.
-    answer = [1, length]
-
-    # 보석의 종류가 몇 개인지 확인하기 위해 set을 사용합니다.
-    check_set = set(gems)
+    length = len(gems)                                  # 보석의 총 길이를 변수에 넣어줍니다.
+    min_length = int(length)                            # 계속 경신 시켜줄 answer 판단용 최소 길이 값 입니다.
+    answer = [1, length]                                # answer로 반환 시킬, [시작 idx, 끝 idx] 의 초기값 입니다.
+    check_set = set(gems)                               # 보석의 종류가 몇 개인지 확인하기 위해 set을 사용합니다.
     count_length = len(check_set)
 
-    # 빠르게 특정 케이스를 토스해주기 위한 로직입니다.
-    # 전체 보석들이 한번 씩만 등장한다면, 바로 answer를 반환합니다.
-    if length == count_length:
-        return answer
-    # 한 종류의 보석으로만 구성되어있을 경우, 바로 [1, 1]에서 완결이 나므로 반환합니다.
-    elif count_length == 1:
-        return [1, 1]
+    if length == count_length:                          # 빠르게 특정 케이스를 토스해주기 위한 로직입니다.
+        return answer                                   # 전체 보석들이 한번 씩만 등장한다면, 바로 answer를 반환합니다.
+    elif count_length == 1:                             # 한 종류의 보석으로만 구성되어있을 경우,
+        return [1, 1]                                   # 바로 [1, 1]에서 완결이 나므로 반환합니다.
 
-    # 각 스트링의 값마다 특정한 id를 배정해줍니다. 이는 나중에 idx로 활용됩니다.
-    check_dict = {}
-
-    # 보석의 종류 별로 idx를 0부터 하나씩 배정해줍니다.
-    idx = 0
-    for gem in check_set:
+    check_dict = {}                                     # 각 스트링의 값마다 특정한 id를 배정해줍니다.
+    idx = 0                                             # 이는 나중에 idx로 활용됩니다.
+    for gem in check_set:                               # 보석의 종류 별로 idx를 0부터 하나씩 배정해줍니다.
         check_dict[gem] = idx
         idx += 1
 
-    # 각 보석별 인덱스를 넣어줄 리스트 입니다. 가장
-    pointers = [length-1]*count_length
+    pointers = [length-1]*count_length                  # 각 보석별 인덱스를 넣어줄 리스트 입니다. 기본값 = 최대값
+    max_idx = length-1                                  # 초기 최대 idx 값은 최후미의 값을 배정합니다.
+    min_idx = 0                                         # 초기 최소 idx 값은 0을 배정합니다.
 
-    # 초기 최대 idx 값은 최후미의 값을 배정합니다.
-    max_idx = length-1
+    visited = set()                                     # 효율화 용 visited 입니다. 모든 보석이 한번씩 등장해야 길이를 측정 시작합니다.
+    count_flag = False                                  # 보석이 한번씩 다 나왔을 때부터 로직에 들어갈 flag 변수입니다.
 
-    # 초기 최소 idx 값은 0을 배정합니다.
-    min_idx = 0
+    for top in range(length):                           # 보석을 한번씩 확인하는 반복문입니다. O(N) 만큼의 연산이 소요됩니다.
+        if not count_flag:                              # 아직 모든 보석들이 한번씩 나오지 않았다면
+            if gems[top] not in visited:                # 등장한 보석이 등장한 적 있는지 확인하고
+                visited.add(gems[top])                  # 없었다면 visited에 넣어줍니다.
+                if len(visited) == count_length:        # 만약 보석들이 한번씩 다 등장했다면,
+                    count_flag = True                   # Flag를 활성화 시키고 로직에 진입합니다.
 
-    # 효율화 용 visited 입니다.
-    # 모든 보석이 한번씩 등장해야 길이를 측정 시작합니다.
-    visited = set()
+        picked_gem = check_dict[gems[top]]              # 각 보석을 뽑을 때마다 적어주기 편하게 하기 위해 변수에 배정해줍니다.
+        picked_gem_idx = pointers[picked_gem]           # 안해도 되지만, 가독성을 위해 변수로 할당해줍니다.
 
-    # 보석이 한번씩 다 나왔을 때부터 로직에 들어갈 flag 변수입니다.
-    # [Tip!] 이런 특정 조건 확인용 변수는 보통 flag로 배정합니다.
-    count_flag = False
+        pointers[picked_gem] = top                      # 현재 뽑은 위치로 보석의 위치를 갱신 시켜줍니다.
 
-    # 보석을 한번씩 확인하는 반복문입니다. O(N) 만큼의 연산이 소요됩니다.
-    for top in range(length):
-
-        # 아직 모든 보석들이 한번씩 나오지 않았다면,
-        if not count_flag:
-            # 등장한 보석이 등장한 적 있는지 확인하고
-            if gems[top] not in visited:
-                # 없었다면 visited에 넣어줍니다.
-                visited.add(gems[top])
-
-                # 만약 보석들이 한번씩 다 등장했다면,
-                if len(visited) == count_length:
-                    # Flag를 활성화 시키고 로직에 진입합니다.
-                    count_flag = True
-
-        # 각 보석을 뽑을 때마다 적어주기 편하게 하기 위해 변수에 배정해줍니다.
-        # 안해도 되지만, 가독성을 위해 변수로 할당해줍니다.
-        picked_gem = check_dict[gems[top]]
-        picked_gem_idx = pointers[picked_gem]
-
-        # 현재 뽑은 위치로 보석의 위치를 갱신 시켜줍니다.
-        pointers[picked_gem] = top
-
-        # 뽑은 보석의 이전 위치가 max_idx, 즉 최대값과 일치했다면
-        if picked_gem_idx == max_idx:
-            # flag가 True 일때 로직에 진입합니다.
-            if count_flag:
-
-                # 최대값은 현재 위치가 됩니다.
-                max_idx = top
-
-                # 최소값은 갱신 되었을 수 있으므로 min으로 한번 확인해줍니다.
-                min_idx = min(pointers)
-
-                # 현재 진열장의 길이를 계산해줍니다.
-                current_length = max_idx - min_idx
-
-                # 현재의 진열장 거리가 최소값보다 작다면,
-                if current_length < min_length:
-                    # 최솟값을 경신해줍니다.
-                    min_length = current_length
-                    # 최솟값이 경신됨에 따라 answer도 갱신해줍니다.
+        if picked_gem_idx == max_idx:                   # 뽑은 보석의 이전 위치가 max_idx, 즉 최대값과 일치했다면
+            if count_flag:                              # flag가 True 일때 로직에 진입합니다.
+                max_idx = top                           # 최대값은 현재 위치가 됩니다.
+                min_idx = min(pointers)                 # 최소값은 갱신 되었을 수 있으므로 min으로 한번 확인해줍니다.
+                current_length = max_idx - min_idx      # 현재 진열장의 길이를 계산해줍니다.
+                if current_length < min_length:         # 현재의 진열장 거리가 최소값보다 작다면,
+                    min_length = current_length         # 최솟값과 answer 을 갱신해줍니다.
                     answer = [min(pointers) + 1, max(pointers) + 1]
 
-        # 뽑은 보석의 이전 위치가 max_idx, 즉 최소값과 일치했다면
-        elif picked_gem_idx == min_idx:
-            # 로직에 진입합니다.
-            if count_flag:
-                # 세부 로직은 위와 일치합니다.
-                max_idx = top
+        elif picked_gem_idx == min_idx:                 # 뽑은 보석의 이전 위치가 max_idx, 즉 최소값과 일치했다면
+            if count_flag:                              # 로직에 진입합니다.
+                max_idx = top                           # 세부 로직은 위와 일치합니다.
                 min_idx = min(pointers)
                 current_length = max_idx - min_idx
                 if current_length < min_length:
--- 2.Algorithm/4. Programmers/피로도ver2.py
+++ 2.Algorithm/4. Programmers/피로도ver2.py
@@ -16,41 +16,19 @@ from itertools import permutations
 # ex) 6개의 던전 중에서 1 3 2 4 순으로 공략하는게 최대 4개를 반환하는 케이스라면
 # -> 1 3 2 4 5 6 도 맞는 패턴이 된다. 또한, 6을 나중에 깰수 없다면, 1 3 2 6 4 5도 맞는 케이스가 된다.
 
-def solution(k, dungeons):
-    # 던전의 총 개수를 저장해준다. 최대 8개 이므로 순열로 풀 수 있다.
-    # [Tip!] 만약 던전 개수가 훨씬 많아진다면, 탐색 과정의 백트래킹과, union-find 알고리즘을 활용해야한다.
-    length = len(dungeons)
-
-    # 갱신해줄 초기 answer 값이다.
-    answer = 0
-
-    # 나올 수 있는 던전의 순서 패턴을 전부 저장해준다.
+def solution(k, dungeons):                 # 던전의 총 개수를 저장해준다. 최대 8개 이므로 순열로 풀 수 있다.
+    length = len(dungeons)                 # [Tip!] 만약 던전 개수가 훨씬 많아진다면, 탐색 과정의 백트래킹과, union-find 알고리즘을 활용해야한다.
+    answer = 0                             # 갱신해줄 초기 answer 값이다.
     pattern_cases = set(permutations([i for i in range(length)], length))
-
-    # 하나씩 뽑으면서 전체 케이스를 확인한다.
-    while pattern_cases:
+    while pattern_cases:                   # 나올 수 있는 던전의 순서 패턴을 전부 저장해준 SET에서 하나씩 뽑아준다.
         pick_case = pattern_cases.pop()
-
-        # 이번 패턴에서의 클리어한 던전 개수를 저장할 변수.
-        clear_count = 0
-
-        # 남은 체력을 의미한다.
-        remain_HP = int(k)
-
-        # idx 기반으로 탐색해준다.
-        for check in range(length):
-
-            # 이번 던전을 진입 가능하면 무조건 진입한다.
-            # 이번 던전을 진입하지 않는것이 효율적이라고 해도, 그 케이스는 다른 패턴에서 다뤄 줄 것이다.
-            if dungeons[pick_case[check]][0] <= remain_HP:
-                # 피로도를 소모해주고 던전 클리어 카운트를 한개 늘려준다.
-                remain_HP -= dungeons[pick_case[check]][1]
+        clear_count = 0                    # 이번 패턴에서의 클리어한 던전 개수를 저장할 변수.
+        remain_HP = int(k)                 # 남은 체력을 의미한다.
+        for check in range(length):        # idx 기반으로 탐색해준다.
+            if dungeons[pick_case[check]][0] <= remain_HP:  # 이번 던전을 진입 가능하면 무조건 진입한다.
+                remain_HP -= dungeons[pick_case[check]][1]  # 피로도를 소모해주고 던전 클리어 카운트를 한개 늘려준다.
                 clear_count += 1
-
-                # 만약 남은 체력이 없으면 바로 종료시킨다.
-                if remain_HP <= 0:
+                if remain_HP <= 0:         # 만약 남은 체력이 없으면 바로 종료시킨다.
                     break
-
-        # answer 를 경신시켜준다.
-        answer = max(clear_count, answer)
+        answer = max(clear_count, answer)  # answer 를 경신시켜준다.
     return answer
